a138a8260956c21fe3c6bd55bca4607facf66cba
floweryclover <floweryclover@gmail.com>
2024-05-08 22:15:51 +0900

    solved boj 1010

--- Cpp/b1010.cpp
+++ Cpp/b1010.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     unsigned long long int t, n, m;
     cin >> t;
-
+    array<array<unsigned long long, 31>, 31> pascal;
+    for (int i=0; i<=30; i++)
+    {
+        pascal[i][0] = 1;
+        for (int j=1; j<i; j++)
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+            
+        pascal[i][i] = 1;
+    }
     for (int i=0; i<t; i++)
     {
         cin >> n;
         cin >> m;
-
-        unsigned long long int sum = 1;
-        for (int j=0; j<n; j++)
-        {
-            sum *= m;
-            m--;
-        }
-
-        for (int j=0; j<n; j++)
-        {
-            sum /= n-j;
-        }
         
-        cout << sum << endl;
+        cout << pascal[m][n] << "\n";
     }
 }
