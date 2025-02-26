887541ed57187751a62a19f85bd576e056723c99
floweryclover <floweryclover@gmail.com>
2025-02-27 00:16:56 +0900

    solved boj 1647

---
+++ Cpp/b1647.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <array>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+
+array<int, 100001> Parents;
+
+int Find(const int node)
+{
+    if (Parents[node] == node)
+    {
+        return node;
+    }
+
+    return Parents[node] = Find(Parents[node]);
+}
+
+bool Union(const int nodeA, const int nodeB)
+{
+    const auto rootA = Find(nodeA);
+    const auto rootB = Find(nodeB);
+    if (rootA != rootB)
+    {
+        Parents[rootB] = rootA;
+        return true;
+    }
+
+    return false;
+}
+
+struct SearchData
+{
+    int Src;
+    int Dst;
+    int Cost;
+
+    explicit SearchData(const int src, const int dst, const int cost)
+    : Src{src},
+    Dst{dst},
+    Cost{cost}
+    {}
+
+    bool operator>(const SearchData& rhs) const
+    {
+        return Cost > rhs.Cost;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+
+    priority_queue<SearchData, vector<SearchData>, greater<SearchData>> priorityQueue;
+
+    for (int i=0; i<M; ++i)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Parents[a] = a;
+        Parents[b] = b;
+
+        priorityQueue.emplace(a, b, c);
+    }
+
+    int totalCost = 0;
+    int mostExpensive = 0;
+    while (!priorityQueue.empty())
+    {
+        const auto currentData = priorityQueue.top();
+        priorityQueue.pop();
+
+        if (!Union(currentData.Src, currentData.Dst))
+        {
+            continue;
+        }
+
+        totalCost += currentData.Cost;
+        mostExpensive = max(mostExpensive, currentData.Cost);
+    }
+
+    cout << totalCost - mostExpensive << endl;
+
+    return 0;
+}
